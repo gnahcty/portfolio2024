69a073c7813f29a61c2d8e3790cdcc1ea650a1f0
gnahcty <gnahcty@gmail.com>
2024-06-09 02:01:06 +0800

    chore: Optimize homepage rendering and scrolling performance

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,60 +1,40 @@
 <template>
-  <!-- <div v-if="!site.AnimeCompleted">
+  <div v-if="!site.AnimeCompleted">
     <IntroAnime></IntroAnime>
   </div>
-  <div v-else> -->
-  <HeroPage class="section"></HeroPage>
-  <AboutMe class="section"></AboutMe>
-  <SkillSet class="section"></SkillSet>
-  <WorkCollection class="section"></WorkCollection>
-  <ContactMe class="section"></ContactMe>
-  <!-- </div> -->
+  <div v-else>
+    <HeroPage class="section"></HeroPage>
+    <AboutMe class="section"></AboutMe>
+    <SkillSet class="section"></SkillSet>
+    <WorkCollection class="section"></WorkCollection>
+    <ContactMe class="section"></ContactMe>
+  </div>
 </template>
 
 <script setup>
 import { onMounted, watch, nextTick } from 'vue'
-import gsap from 'gsap'
-import ScrollTrigger from 'gsap/ScrollTrigger'
 import { useGeneralStore } from '@/stores/data'
+import { scrollToTop, sectionTrigger } from '@/animation/animations.js'
 
-gsap.registerPlugin(ScrollTrigger)
 const site = useGeneralStore()
 
 onMounted(() => {
   // signal loading is done
   site.isLoading = false
 
-  // Scroll to the top of the page when the page is loaded
-  gsap.to(window, { duration: 0, scrollTo: 0 })
+  // scroll to top when reloaded
+  scrollToTop()
 
-  // Scroll to the top of the page when the page is refreshed
-  window.onbeforeunload = () => {
-    window.scrollTo(0, 0)
-  }
-
-  // Change the active section based on the scroll position
+  // create section trigger after intro animation finished and rest of DOM is rendered
   watch(
     () => site.AnimeCompleted,
     (state) => {
       if (state === true) {
         nextTick(() => {
-          gsap.utils.toArray('.section').forEach((section, i) => {
-            ScrollTrigger.create({
-              trigger: section,
-              start: 'top top',
-              end: 'bottom top',
-              onEnter: () => {
-                site.section.activeSection = i
-              },
-              onLeaveBack: () => {
-                site.section.activeSection = i - 1
-              }
-            })
-          })
+          sectionTrigger(site)
         })
       }
     }
   )
 })
 </script>
-<style scoped></style>
